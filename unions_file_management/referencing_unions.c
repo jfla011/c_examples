021e51efa56dbf1fa475ea9f914a96faae8ddf60
Jason Flaherty <flaherty.jason@gmail.com>
2021-05-15 13:12:39 +0200

    referencing unions

---
+++ unions_file_management/referencing_unions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    typedef union InternalRepresentaiton {
+        int integer;
+        float floating;
+    } InternalRepresentaiton;
+
+    InternalRepresentaiton ir = {0};
+    InternalRepresentaiton *irp = &ir;
+
+    ir.floating = 123.456;
+    printf("%g %i\n", ir.floating, ir.integer);
+
+    irp->integer = 1234567890;
+    printf("%g %i\n", ir.floating, ir.integer);
+
+}
